3124192c145f8063a37726877c5795bdae65ed0b
Octavi Obiols <octaviobiols@MacBook-Pro.local>
2021-06-15 16:28:13 +0200

    adding Conv-Attention model

--- src/NS_attention.py
+++ src/NS_attention.py
@@ -19,50 +19,75 @@ class PositionEmbedding(keras.layers.Layer):
 
 #class Masking
 
-class ResidualBlock(keras.layers.Layer):
+class ResidualBlockAttentionModule(keras.layers.Layer):
     def __init__(self,
-                 filters=16,
-                 kernel_size=5,
+                 f=16,
+                 r=4,
+                 height=64,
+                 width=256,
                  **kwargs):
 
-        super(ResidualBlock,self).__init__(**kwargs)
+        super(ResidualBlockAttentionModule,self).__init__(**kwargs)
         
-        self.Conv1 = keras.layers.Conv2D(filters=filters,
-                                         kernel_size=(kernel_size,kernel_size),
-                                         strides=(1,1),
-                                         activation=tf.nn.leaky_relu,
-                                         padding="same")
+        self.SA = SpatialAttentionModule(f=f,r=r)
+        self.elementwise_1 = keras.layers.Multiply()
+        self.CA = ChannelAttentionModule(f=f,r=r,height=height,width=width)
+        self.elementwise_2 = keras.layers.Multiply()
+        self.res = keras.layers.Add()
 
-        self.BN1 = keras.layers.BatchNormalization(axis=-1)
+    def call(self,inputs):
+        
+       spatial = self.SA(inputs)
+       spatial_attention = self.elementwise_1([spatial,inputs])
+       channel = self.CA(spatial_attention)
+       channel_attention = self.elementwise_1([channel,spatial_attention])
+       x = self.res([inputs,channel_attention])
+      
+       return x
 
-        self.Conv2 = keras.layers.Conv2D(filters=filters,
-                                         kernel_size=(kernel_size,kernel_size),
-                                         strides=(1,1),
-                                         activation=tf.nn.leaky_relu,
-                                         padding="same")
+class SpatialAttentionModule(keras.layers.Layer):
+    def __init__(self,
+                 f=16 ,
+                 r=4 ,
+                 **kwargs):
 
-        self.BN2 = keras.layers.BatchNormalization(axis=-1)
-
-        self.Conv3 = keras.layers.Conv2D(filters=filters,
-                                         kernel_size=(kernel_size,kernel_size),
-                                         strides=(1,1),
-                                         activation=tf.nn.leaky_relu,
-                                         padding="same")
-
-        self.BN3 = keras.layers.BatchNormalization(axis=-1)
-
-        self.Add = keras.layers.Add()
+        super(SpatialAttentionModule,self).__init__(**kwargs)
+        
+        c = int(f/r)
+        self.reduction = keras.layers.Conv2D(filters=c,kernel_size=(1,1),strides=(1,1),padding="same",activation=tf.nn.leaky_relu)
+        self.dilated = keras.layers.Conv2D(filters=c,kernel_size=(5,5),strides=(1,1),padding="same",activation=tf.nn.leaky_relu)
+        self.spatial = keras.layers.Conv2D(filters=1,kernel_size=(1,1),strides=(1,1),padding="same",activation=tf.nn.leaky_relu)
+        
 
     def call(self,inputs):
 
-       x1 = self.Conv1(inputs)
-       x = self.BN1(x1)
-     #  x = self.Conv2(x)
-    #   x = self.BN2(x)
-#       x = self.Conv3(x)
-#       x = self.BN3(x)
-       x = self.Add([x,x1])
+       x = self.reduction(inputs)
+       x = self.dilated(x)
+       x = self.spatial(x)
+      
+       return x
 
+class ChannelAttentionModule(keras.layers.Layer):
+    def __init__(self,
+                 filters=16,
+                 r = 4 ,
+                 height = 16,
+                 width  = 64, 
+                 **kwargs):
+
+        super(ChannelAttentionModule,self).__init__(**kwargs)
+        
+        c = int(f/r)
+        self.pooling = keras.layers.AveragePooling2D(pool_size=(height,width),strides=(height,width))
+        self.mlp1 = keras.layers.Dense(c,activation=tf.nn.leaky_relu)
+        self.mlp2 = keras.layers.Dense(f,activation=tf.nn.leaky_relu)
+         
+    def call(self,inputs):
+
+       x = self.pooling(inputs)
+       x = self.mlp1(x)
+       x = self.mlp2(x)
+      
        return x
 
 class NSAttention(NSModelPinn):
@@ -72,8 +97,8 @@ class NSAttention(NSModelPinn):
                factor = 4,
                strides= 2,
                kernel_size = 5,
-               query_dimension = 100,
-               value_dimension = 100,
+               num_attention = 1,
+               attention = True,
                **kwargs):
 
     super(NSAttention, self).__init__(**kwargs)
@@ -82,50 +107,40 @@ class NSAttention(NSModelPinn):
     self.value_dim = value_dimension
     self.HR_size = image_size
     self.LR_size = [int(image_size[0]/self.f),int(image_size[1]/self.f)]
-
-    self.res_block = []
-    self.pooling = []
-    self.invpooling = []
-    self.concatenate = []
-    self.conv = []
-    self.deconv = []
+    self.num_attention = num_attention
+    self.attention = attention
+    self.feature_extractor = []
+    self.reconstruction = []
+    self.RBAM = []
 
     self.k = kernel_size
     self.s = strides
 
     self.upsample = keras.layers.UpSampling2D(size=(self.f,self.f),interpolation="bilinear")
-    #self.downsample = keras.layers.AveragePooling2D(pool_size=(self.f,self.f),strides=(self.f,self.f),padding="same")
 
   
     for i in filters:
-        self.conv.append(keras.layers.Conv2D(filters=i,
+        self.feature_extractor.append(keras.layers.Conv2D(filters=i,
                                              kernel_size=(self.k,self.k),
                                              strides=(self.s,self.s),
 						padding="same",
                                              activation=tf.nn.leaky_relu))
     
     for i in reversed(filters):
-        self.deconv.append(keras.layers.Conv2DTranspose(filters=i,           
+        self.reconstruction.append(keras.layers.Conv2DTranspose(filters=i,           
                                              kernel_size=(self.k,self.k),
                                              strides=(self.s,self.s),
-							padding="same",
+					     padding="same",
                                              activation=tf.nn.leaky_relu))
  
-    #self.query_encoding = keras.layers.Conv2D(filters=query_dimension,
-    #                                            kernel_size=(self.LR_size[0],self.LR_size[1]),
-    #                                            strides=(self.LR_size[0],self.LR_size[1]),
-    #                                            padding="same")
+     fil = filters[-1]  
 
-    #self.value_encoding = keras.layers.Conv2D(filters=value_dimension,
-    #                                            kernel_size=(4,4), 
-    #                                            strides=(4,4),
-    #                                            padding="same")
+     for i in self.num_attention:
+         self.RBAM.append(ResidualBlockAttentionModule(filters=fil,
+                                                        r=4,
+                                                        height=self.HR_size[0],
+                                                        width=self.LR_size[1]))
 
-
-    #self.attention = keras.layers.Attention()
-    #self.reshape_query = keras.layers.Reshape((-1,self.query_dim))
-    #self.reshape_value = keras.layers.Reshape((-1,self.value_dim))
-    self.concatenate_coordinates = keras.layers.Concatenate(axis=-1)
 
   def call(self, inputs):
 
@@ -134,45 +149,41 @@ class NSAttention(NSModelPinn):
 
     x1 = self.upsample(low_res_true)
     x1 = self.concatenate_coordinates([x1,coordinates])
-    for layer in self.conv:
+    for layer in self.feature_extractor:
         x1 = layer(x1)
+    
+    x2 = x1
+    if self.attention == True:
+     for layer in self.RBAM:
+        x2 = layer(x2)
 
-    for layer in self.deconv:
-        x1 = layer(x1)
+    x3 = x2
+    for layer in self.reconstruction:
+        x3 = layer(x3)
 
 
-    high_res_pred = x1
+    high_res_pred = x3
     low_res_pred = tf.image.resize(high_res_pred,
                                    size = self.LR_size,
                                    method='bilinear',
                                    preserve_aspect_ratio = False)
 
-#    low_res_pred = self.downsample(high_res_pred)
-#    diff = tf.math.square(low_res_pred-low_res_true)
-#
-#    query = self.extractor(diff)
-#    value   = self.extractor(high_res_pred)
-#
-#    query_enc = self.query_encoding(query)
-#    value_enc = self.value_encoding(value)
-#
-#
-#    query_enc = self.reshape_query(query_enc)
-#    value_enc = self.reshape_value(value_enc)
-#
-#    attention , scores = self.attention([query_enc,value_enc],return_attention_scores=True)  
-
     return high_res_pred , low_res_pred
 
 
-  def compute_data_pde_losses(self, low_res_true, high_res_xz,labels):
+  def compute_data_pde_losses(self, high_res_true, high_res_xz,labels):
+
+    low_res_true = tf.image.resize( high_res_true,
+                                    size=self.LR_size,
+                                    method="bilinear",
+                                    preserve_aspect_ratio=True)
 
     with tf.GradientTape(watch_accessed_variables=False,persistent=True) as tape2:
       tape2.watch(high_res_xz)
       with tf.GradientTape(watch_accessed_variables=False,persistent=True) as tape1:
         tape1.watch(high_res_xz)
 
-        high_res_pred , low_res_pred = self([low_res_true,high_res_xz])
+        high_res_pred = self([low_res_true,high_res_xz])
 
         u_pred_LR       = low_res_pred[:,:,:,0]
         v_pred_LR       = low_res_pred[:,:,:,1]
@@ -200,10 +211,11 @@ class NSAttention(NSModelPinn):
     v_zz = tape2.gradient(v_z, high_res_xz)[:,:,:,1]
     del tape2
 
-    uMse = tf.reduce_mean(tf.square(u_pred_LR - low_res_true[:,:,:,0]))
-    vMse = tf.reduce_mean(tf.square(v_pred_LR - low_res_true[:,:,:,1]))
-    pMse = tf.reduce_mean(tf.square(p_pred_LR -  low_res_true[:,:,:,2]))
-    nuMse = tf.reduce_mean(tf.square(nu_pred_LR - low_res_true[:,:,:,3]))
+    uMse = tf.reduce_mean(tf.square(u_pred_LR - low_res_true[:,:,:,0])) + tf.reduce_mean(tf.square(u_pred_HR[:,59:64,:]-high_res_true[:,59:64,:,0]))
+    vMse = tf.reduce_mean(tf.square(v_pred_LR - low_res_true[:,:,:,1])) + tf.reduce_mean(tf.square(v_pred_HR[:,59:64,:]-high_res_true[:,59:64,:,1]))
+    pMse = tf.reduce_mean(tf.square(p_pred_LR -  low_res_true[:,:,:,2]))+ tf.reduce_mean(tf.square(p_pred_HR[:,59:64,:]-high_res_true[:,59:64,:,2]))
+    nuMse = tf.reduce_mean(tf.square(nu_pred_LR - low_res_true[:,:,:,3]))+ tf.reduce_mean(tf.square(nu_pred_HR[:,59:64,:]-high_res_true[:,59:64,:,3]))
+
 
     # pde error, 0 continuity, 1-2 NS
     pde0    = u_x + v_z
@@ -213,6 +225,8 @@ class NSAttention(NSModelPinn):
     pde1    = u_pred_HR*u_x + v_pred_HR*u_z + p_x - (0.01+ nu_pred_HR)*(1/(6000*500))*(u_xx + u_zz)
     pde1Mse    = tf.reduce_mean(tf.square(pde1-z))
     #pde1Mse = 0 
+
+
    # pde2    = u_pred_HR*v_x + v_pred_HR*v_z + p_z - (0.01 + nu_pred_HR)*(1/6000)*(v_xx + v_zz)
    # pde2Mse    = tf.reduce_mean(tf.square(pde2-z))
     pde2Mse = 0 
@@ -275,16 +289,12 @@ class NSAttention(NSModelPinn):
     high_res_true = inputs[:,:,:,0:4]
     high_res_xz = inputs[:,:,:,4:6] 
     
-    low_res_true = tf.image.resize( high_res_true,
-                                    size=self.LR_size,
-                                    method="bilinear",
-                                    preserve_aspect_ratio=True)
 
     with tf.GradientTape(persistent=True) as tape0:
       # compute the data loss for u, v, p and pde losses for
       # continuity (0) and NS (1-2)
       uMse, vMse, pMse, nuMse, contMse, momxMse, momzMse = \
-        self.compute_data_pde_losses(low_res_true, high_res_xz,labels)
+        self.compute_data_pde_losses(high_res_true, high_res_xz,labels)
       # replica's loss, divided by global batch size
       data_loss  = 0.5*(uMse   + vMse)# + pMse + nuMse) 
 
