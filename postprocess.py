5c818ccfa9f931f363913b3d4b22e25a1405f423
Octavi Obiols <octaviobiols@MacBook-Pro.local>
2021-08-24 11:20:13 +0200

    updating postprocess

--- postprocess.py
+++ postprocess.py
@@ -5,78 +5,77 @@ from mpl_toolkits.mplot3d import Axes3D
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--name', type=str, default='name',\
-                    help='batch size for model.predict')
+parser.add_argument('-mn', '--modelname', type=str, default='name',\
+                    help='modelname')
 parser.add_argument('-nb', '--numberbins', type=int, default=4,\
                     help='batch size for model.predict')
-parser.add_argument('-he', '', type=int, default=16,\
+parser.add_argument('-he', '--height', type=int, default=32,\
                     help='batch size for model.predict')
+parser.add_argument('-w', '--width', type=int, default=128,\
+                    help='batch size for model.predict')
+parser.add_argument('-ph', '--patchheight', type=int, default=8,\
+                    help='batch size for model.predict')
+parser.add_argument('-pw', '--patchwidth', type=int, default=32,\
+                    help='batch size for model.predict')
+parser.add_argument('-vn', '--variablename', type=str, default='xvelocity',\
+                    help='name of the variable to be drawn')
 
 args = parser.parse_args()
-true_data = np.load('cylinder_lr.npy')[0:1]
+true_data = np.load('channelflow_lr_turb.npy')[0:1]
 
-umax = np.max(true_data[0,:,:,0])
-umin = np.min(true_data[0,:,:,0])
-print(umax)
-print(umin)
 
-p0 = np.load('saved_patches/patch_0.npy')
-p1 = np.load('saved_patches/patch_1.npy')
+if (args.variablename == "xvelocity"):
+    v = 0
+if (args.variablename == "yvelocity"):
+    v = 1
+if (args.variablename == "pressure"):
+    v = 2
+if (args.variablename == "nutilda"):
+    v = 3
 
-i0 = np.load('saved_indices/idx_0.npy')[:,0]
-i1 = np.load('saved_indices/idx_1.npy')[:,0]
+npx = args.height // args.patchheight
+npy = args.width // args.patchwidth
 
-print(i0)
-print(i1)
+np = npx*npy
 
-fig, axs = plt.subplots(4, 4 , gridspec_kw = {'wspace':0, 'hspace':-0.9})
+loaded_patches = []
+loaded_indices = []
 
-for i in range(16):
+for i in range(args.numberbins):
 
- if i in i0:
-  idx = np.where(i0==i)
-  idx=idx[0][0]
-  patch = p0[idx,:,:,:]
+    loaded_patches.append(np.load('saved_patches/patch_'+str(i)+'.npy'))
+    loaded_indices.append(np.load('saved_indices/idx_'+str(i)+'.npy'))
+    print(loaded_indices[i])
+
+
+fig, axs = plt.subplots(npx, npy , gridspec_kw = {'wspace':0, 'hspace':-0.9})
+
+for i in range(np):
+ for j , indices in enumerate(loaded_indices):
+
+     if i in indices:
+         idx = np.where(indices==i)
+         idx = idx[0][0]
+         patches = loaded_patches[j]
+         patch = patches[idx,:,:,:]
+
+  x = i//npx
+  y = i%npy
+
+  data = patch[:,:,v]
+  axs[x,y].set_xticks([])
+  axs[x,y].set_yticks([])    
+  im = axs[x,y].imshow(np.float32(data),interpolation='none')
+  im.set_clim(umin,umax)
   
- elif i in i1:
-  idx = np.where(i1==i)
-  idx=idx[0][0]
-  patch = p1[idx,:,:,:]
 
- x = i//4
- y = i%4
-  
- data = patch[:,:,0]
- axs[x,y].set_xticks([])
- axs[x,y].set_yticks([])    
- im = axs[x,y].imshow(np.float32(data),interpolation='none')
- im.set_clim(umin,umax)
-  
-plt.savefig(args.name+'.png')
+directory_name = './amr_fields/'+args.modelname
+file_name = args.variablename
+
+if not os.path.exists(directory_name):
+    os.makedirs(directory_name)
+
+plt.savefig(directory_name+'/'+file_name)
 plt.close()
   
   
-##fig, axs = plt.subplots(4, 4 , gridspec_kw = {'wspace':0, 'hspace':-0.9})
-###plt.subplots_adjust(wspace=0,hspace=-0.1)
-##for i in range(ux_patches.shape[1]):
-## for j in range(ux_patches.shape[2]):
-##
-##    data = ux_patches[0,i,j,:]
-##    data = data.reshape((8,32))
-##    axs[i,j].set_xticks([])
-##    axs[i,j].set_yticks([])    
-##    im = axs[i,j].imshow(np.float32(data),interpolation='none')
-##    im.set_clim(umin,umax)
-##    #plt.colorbar(im, ax = axs[i,j])
-##
-###plt.subplots_adjust(wspace=0, hspace=0)
-##plt.savefig('./test.png')
-##plt.close()
-##
-##fig, axs = plt.subplots()
-##im = axs.imshow(np.float32(ux[0,:,:,0]),interpolation='none')
-##im.set_clim(umin,umax)
-##plt.savefig('./test_2.png')
-
-
-
